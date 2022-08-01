bcdc89ba9fb9b6854d9044b7e9cebada3690d092
Andrés Contreras <ingluisandres3@gmail.com>
2022-07-31 22:26:25 -0500

    Termino el tutorial

--- lib/ex_poll_web/controllers/vote_controller.ex
+++ lib/ex_poll_web/controllers/vote_controller.ex
@@ -3,41 +3,17 @@ defmodule ExPollWeb.VoteController do
 
   alias ExPoll.Polls
   alias ExPoll.Polls.Vote
+  alias ExPollWeb.Endpoint
 
   action_fallback ExPollWeb.FallbackController
 
-  def index(conn, _params) do
-    votes = Polls.list_votes()
-    render(conn, "index.json", votes: votes)
-  end
-
-  def create(conn, %{"vote" => vote_params}) do
-    with {:ok, %Vote{} = vote} <- Polls.create_vote(vote_params) do
+  def create(conn, %{"id" => id, "vote" => %{"option_id" => option_id}}) do
+      option = Polls.get_option!(option_id)
+      with {:ok, %Vote{} = vote} <- Polls.create_vote(option) do
+          Endpoint.broadcast!("poll:" <> id, "new_vote", %{option_id: option.id})
       conn
       |> put_status(:created)
-      |> put_resp_header("location", Routes.vote_path(conn, :show, vote))
       |> render("show.json", vote: vote)
-    end
-  end
-
-  def show(conn, %{"id" => id}) do
-    vote = Polls.get_vote!(id)
-    render(conn, "show.json", vote: vote)
-  end
-
-  def update(conn, %{"id" => id, "vote" => vote_params}) do
-    vote = Polls.get_vote!(id)
-
-    with {:ok, %Vote{} = vote} <- Polls.update_vote(vote, vote_params) do
-      render(conn, "show.json", vote: vote)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    vote = Polls.get_vote!(id)
-
-    with {:ok, %Vote{}} <- Polls.delete_vote(vote) do
-      send_resp(conn, :no_content, "")
     end
   end
 end
--- lib/ex_poll_web/views/option_view.ex
+++ lib/ex_poll_web/views/option_view.ex
@@ -12,8 +12,9 @@ defmodule ExPollWeb.OptionView do
 
   def render("option.json", %{option: option}) do
     %{
-      id: option.id,
-      value: option.value
-    }
+        id: option.id,
+        value: option.value,
+        vote_count: option.vote_count
+      }
   end
 end
